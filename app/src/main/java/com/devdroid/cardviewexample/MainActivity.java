068512bee6f4ab9a6399bddf24eb0057ccead4d2
shubh <shubhanshuagnihotri16@gmail.com>
2024-03-07 18:39:19 +0530

    CardViewExample

---
+++ app/src/main/java/com/devdroid/cardviewexample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.devdroid.cardviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.cardView);
+        cardView.setRadius(5.0f);
+        cardView.setCardElevation(11.0f);
+        cardView.setUseCompatPadding(true);
+    }
+}
